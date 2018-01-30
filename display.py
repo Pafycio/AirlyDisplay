aab5c88a8fd8fbf2678381564903283acf188c02
Lynx <fertpawel@gmail.com>
2018-01-30 19:27:36 +0100

    convert to string

--- display.py
+++ display.py
@@ -31,13 +31,13 @@ class Display(object):
 
     def writeTempPm(self):
         self.lcd.move_to(0, 1)
-        self.lcd.putstr(self.result.temperature)
+        self.lcd.putstr(str(self.result.temperature))
         self.lcd.putchar(0)
         self.lcd.putstr(' ')
-        self.lcd.putstr(self.result.pm10)
+        self.lcd.putstr(str(self.result.pm10))
         self.lcd.putchar(4)
         self.lcd.putstr(' ')
-        self.lcd.putstr(self.result.pm25)
+        self.lcd.putstr(str(self.result.pm25))
         self.lcd.putchar(2)
         self.lcd.putchar(3)
 
