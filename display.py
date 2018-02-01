8b4f042eb96cc2c5f820ed1b8b9e2eba797dc88b
Lynx <fertpawel@gmail.com>
2018-02-01 22:34:47 +0100

    new view

--- display.py
+++ display.py
@@ -14,6 +14,8 @@ two = bytearray([0x00, 0x00, 0x08, 0x14, 0x04, 0x08, 0x10, 0x1D])
 five = bytearray([0x00, 0x00, 0x1C, 0x10, 0x18, 0x04, 0x04, 0x18])
 ten = bytearray([0x00, 0x00, 0x12, 0x15, 0x15, 0x15, 0x15, 0x12])
 
+h = bytearray([0x00, 0x00, 0x11, 0x11, 0x11, 0x1D, 0x15, 0x15])
+pa = bytearray([0x00, 0x00, 0x18, 0x08, 0x18, 0x06, 0x0A, 0x0F])
 
 class Display(object):
     def __init__(self, request_handler):
@@ -28,20 +30,31 @@ class Display(object):
         self.lcd.custom_char(2, two)
         self.lcd.custom_char(3, five)
         self.lcd.custom_char(4, ten)
+        self.lcd.custom_char(5, h)
+        self.lcd.custom_char(6, pa)
 
-    def writeDateTime(self):
+    def writeDateTime(self, line):
         """Print TIME on first line"""
-        self.lcd.move_to(0, 0)
+        self.lcd.move_to(0, line)
         self.addTime()
 
-    def writeTempPm(self):
+    def writeTempPm(self, line):
         """Print TEMP PM10 PM25 on second line"""
-        self.lcd.move_to(0, 1)
+        self.lcd.move_to(0, line)
         self.addTemp()
         self.addSpace()
         self.addPM10()
         self.addSpace()
         self.addPM25()
+
+    def writeTempHumPress(self, line):
+        """Print TEMP PM10 PM25 on second line"""
+        self.lcd.move_to(0, line)
+        self.addTemp()
+        self.addSpace()
+        self.addHumidity()
+        self.addSpace()
+        self.addPressure()
 
     def addTime(self):
         """HH:MM DD-MM-YYYY"""
@@ -51,6 +64,17 @@ class Display(object):
         """(-)temp(C)"""
         self.lcd.putstr(str(self.result.temperature))
         self.lcd.putchar(chr(0))
+
+    def addHumidity(self):
+        """hum(%)"""
+        self.lcd.putstr(str(self.result.humidity))
+        self.lcd.putstr("%")
+
+    def addPressure(self):
+        """press(hP)(a)"""
+        self.lcd.putstr(str(self.result.pressure))
+        self.lcd.putchar(chr(5))
+        self.lcd.putchar(chr(6))
 
     def addPM10(self):
         """pm10(pm)(10)"""
@@ -76,10 +100,11 @@ class Display(object):
         while True:
             self.updateResult()
             self.lcd.clear()
-            self.writeDateTime()
-            self.writeTempPm()
-
-            time.sleep(30)
+            self.writeDateTime(0)
+            self.writeTempPm(1)
+            time.sleep(15)
+            self.writeTempHumPress(1)
+            time.sleep(15)
 
 
 @begin.start
